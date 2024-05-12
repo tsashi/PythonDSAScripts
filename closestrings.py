aab0605f732764be4daab90f194c0e68609a5a5a
Sashi <tsashi@gmail.com>
2024-05-11 20:11:34 -0400

    Simplified Soln LeedCode 1657

--- closestrings.py
+++ closestrings.py
@@ -3,26 +3,21 @@ from collections import Counter
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
         print("1.--- %s seconds ---" % (time.time() - start_time))
-        lenw1 = len(word1)
-        lenw2 = len(word2)
-        if len(word1) != len(word2):
-            return False
-        print("2.--- %s seconds ---" % (time.time() - start_time))
         # word1freq = {x: word1.count(x) for x in word1}
         # word2freq = {x: word2.count(x) for x in word2}
         word1freq = Counter(word1)
         word2freq = Counter(word2)
-        print("3.--- %s seconds ---" % (time.time() - start_time))
+        print("2.--- %s seconds ---" % (time.time() - start_time))
         
         word1charset = set(word1freq.keys())
         word2charset = set(word2freq.keys())
-        print("4.--- %s seconds ---" % (time.time() - start_time))
-        if (len(word1charset-word2charset) > 0) or (len(word2charset-word1charset) > 0):
+        print("3.--- %s seconds ---" % (time.time() - start_time))
+        if (word1charset != word2charset):
             return False
-        print("5.--- %s seconds ---" % (time.time() - start_time))
+        print("4.--- %s seconds ---" % (time.time() - start_time))
         sortedfreq1 = sorted(word1freq.values())
         sortedfreq2 = sorted(word2freq.values())
-        print("6.--- %s seconds ---" % (time.time() - start_time))
+        print("5.--- %s seconds ---" % (time.time() - start_time))
         if sortedfreq1 != sortedfreq2:
             return False
         
